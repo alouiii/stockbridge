dd262a07eac14cd83c1fbb1df72fd5dfe00d56be
alouiii <achref.aloui73@gmail.com>
2023-06-08 16:10:45 +0200

    refine bar

--- client/src/components/ContentTabs/ContentTab.tsx
+++ client/src/components/ContentTabs/ContentTab.tsx
@@ -7,9 +7,18 @@ type Props = {
 }
 
 
-const ContentTab: React.FC<Props> = ({ children } ) => {
-  return <div>{children}</div>
+const ContentTab: React.FC<Props> = ({ children }) => {
+  return <ul style={{
+    listStyleType: 'none',
+    overflow: 'hidden',
+    marginTop: "5em",
+    flex: "column",
+    gap: 10,
+  }}
+  >
+
+
+    {children}</ul>
 }
 
 export default ContentTab
-  
