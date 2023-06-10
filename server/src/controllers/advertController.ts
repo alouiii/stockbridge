4eb922a951efa6d8ccd56ab579fa9f68a5f47735
alouiii <achref.aloui73@gmail.com>
2023-06-10 14:12:24 +0200

    Adapts the Advert CRUD -- Adds finding adverts by category

--- server/src/controllers/advertController.ts
+++ server/src/controllers/advertController.ts
@@ -1,15 +1,16 @@
 import { type Response } from 'express';
 import asyncHandler from "express-async-handler"
-import {findAdvertById, createAdvert, updateAdvert, delAdvert, findAllAdverts} from "../services/advertServices";
+import {findAdvertById, createAdvert, updateAdvert, delAdvert, findAllAdverts, getAdvertsByCategory} from "../services/advertServices";
 import { AuthenticatedRequest } from "../middlewares/authMiddleware";
 import {AppError} from "../utils/errorHandler";
+import { ProductCategory } from '../entities/advertEntity';
 
 
 /**
- * This method returns a advert by id   *
+ * This method returns an advert by id   *
  * @param req - The request object
  * @param res - The response object
- * @returns a advert object.
+ * @returns an advert object.
  */
 export const getAdvert = asyncHandler(async (req: AuthenticatedRequest, res: Response) => {
     const {id} = req.params;
@@ -41,7 +42,7 @@ export const postAdvert = asyncHandler(async (req: AuthenticatedRequest, res: Re
 });
 
 /**
- * This method updates a advert by id   *
+ * This method updates an advert by id   *
  * @param req - The request object
  * @param res - The response object
  * @returns updated advert object.
@@ -58,7 +59,7 @@ export const putAdvert = asyncHandler(async (req: AuthenticatedRequest, res: Res
 });
 
 /**
- * This method deletes a advert by id   *
+ * This method deletes an advert by id   *
  * @param req - The request object
  * @param res - The response object
  * @returns deleted advert object.
@@ -73,3 +74,15 @@ export const deleteAdvert = asyncHandler(async (req: AuthenticatedRequest, res: 
     const advert = await delAdvert(id);
     res.status(204).json(advert);
 });
+
+/**
+ * This method gets all adverts of a specific category   *
+ * @param req - The request object
+ * @param res - The response object
+ * @returns deleted advert object.
+ */
+export const getAllAdvertsByCategory = asyncHandler(async (req: AuthenticatedRequest, res: Response) => {
+    const {category} = req.params;
+    const adverts = await getAdvertsByCategory(category as ProductCategory);
+    res.status(200).json(adverts);
+})
--- server/src/routes/adverts.ts
+++ server/src/routes/adverts.ts
@@ -1,8 +1,12 @@
 import { Router } from "express";
-import { deleteAdvert, getAdvert, getAdverts, postAdvert, putAdvert } from "../controllers/advertController";
+import { deleteAdvert, getAdvert, getAdverts, getAllAdvertsByCategory, postAdvert, putAdvert } from "../controllers/advertController";
 
 export const advertRouter = Router();
 
 advertRouter.route("/").post(postAdvert).get(getAdverts);
 
 advertRouter.route("/:id").get(getAdvert).put(putAdvert).delete(deleteAdvert);
+
+advertRouter.route('/getAdvertsByCategory/:cat')
+    .get(getAllAdvertsByCategory)
+
