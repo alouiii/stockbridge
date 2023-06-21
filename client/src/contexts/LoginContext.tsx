175b97d2eb53e59edcc0394f6bba7b636e917c46
Bilel <ge49gej@mytum.de>
2023-06-21 22:44:29 +0200

    - fix bug where userInfo doesnt load

--- client/src/contexts/LoginContext.tsx
+++ client/src/contexts/LoginContext.tsx
@@ -42,7 +42,7 @@ export const LoginContextProvider = ({
         })
         .catch(() => {
           setLoggedIn(false);
-          setLoggedIn(false);
+          setIsLoading(false);
           setUser(undefined);
         });
     };
