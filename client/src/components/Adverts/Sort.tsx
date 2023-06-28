61f6213814a113e66b11600b73f2ade11bc30470
Matteo Luppi <100372313+lups2000@users.noreply.github.com>
2023-06-28 11:09:17 +0200

    add state dropdown

--- client/src/components/Adverts/Sort.tsx
+++ client/src/components/Adverts/Sort.tsx
@@ -1,20 +1,43 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import { Dropdown } from 'react-bootstrap';
 import { palette } from '../../utils/colors';
 
+enum SortTypes {
+  NONE = 'None',
+  PRICE_ASC = 'Price Ascending',
+  PRICE_DESC = 'Price Descending',
+  QUANTITY_ASC = 'Quantity Ascending',
+  QUANTITY_DESC = 'Quantity Descending',
+  DATE_ASC = 'Creation Date Ascending',
+  DATE_DESC = 'Creation Date Descending',
+}
+
 export const Sort: FC = () => {
+  const [sortingType, setSortingType] = useState<string>('None');
+
+  const handleClick = (type: SortTypes) => {
+    //set new state and make call to api
+    setSortingType(type);
+  };
+
   return (
     <Dropdown>
-      <Dropdown.Toggle style={{backgroundColor: palette.subSectionsBgAccent, border: "none"}} id="dropdown-basic">
-        Sort By: 
+      <Dropdown.Toggle
+        style={{ backgroundColor: palette.subSectionsBgAccent, border: 'none' }}
+        id="dropdown-basic"
+      >
+        Sort By: {sortingType}
       </Dropdown.Toggle>
       <Dropdown.Menu>
-        <Dropdown.Item href="#/action-1">Price Ascending</Dropdown.Item>
-        <Dropdown.Item href="#/action-2">Price Descending</Dropdown.Item>
-        <Dropdown.Item href="#/action-1">Quantity Ascending</Dropdown.Item>
-        <Dropdown.Item href="#/action-2">Quantity Descending</Dropdown.Item>
-        <Dropdown.Item href="#/action-3">Date Ascending</Dropdown.Item>
-        <Dropdown.Item href="#/action-3">Date Descending</Dropdown.Item>
+        {Object.values(SortTypes)
+          .map((value) => value)
+          .map((type) => {
+            return (
+              <Dropdown.Item onClick={() => handleClick(type)}>
+                {type}
+              </Dropdown.Item>
+            );
+          })}
       </Dropdown.Menu>
     </Dropdown>
   );
