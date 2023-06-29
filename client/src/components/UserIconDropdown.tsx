8a580d831665d8ac4508b86047411adb850cc14b
alouiii <achref.aloui73@gmail.com>
2023-06-29 22:57:35 +0200

    improves smoothness

--- client/src/components/UserIconDropdown.tsx
+++ client/src/components/UserIconDropdown.tsx
@@ -84,13 +84,26 @@ export const UserIconDropdown: FC = () => {
 
   const toggleDropDown = () => setDropdownOpen(!isDropdownOpen);
 
+  /** Autoclicks the selected tab section -- navigating through links requires  
+  * reloading the page which breaks the context.
+  */ 
+  const autoClickTab = (index : number) => {
+    const tabButton = document.getElementsByClassName("profile-section-tab-button")[index] as HTMLElement;
+    tabButton.click();
+  }
+
   const handleItemClick = (item: DropdownItem) => {
     if (item.type === DropdownItemType.LOGOUT) {
       // Handle logout click
       handleLogoutClick();
     } else {
+      // Checks that the current page is userInfo
+      if (window.location.pathname.includes('/userInfo')) {
+        autoClickTab(item.index);
+      }
+
       navigate(`/userInfo?${item.link}`);
-      window.location.reload();
+      //window.location.reload();
     }
     setDropdownOpen(false); // Close dropdown
   };
