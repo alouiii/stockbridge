b1b8de3c243746d6281fddf1dc9b70221f9178ca
Matteo Luppi <100372313+lups2000@users.noreply.github.com>
2023-07-04 22:09:24 +0200

    change to leaflet

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -24,7 +24,7 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/contactUs" element={<ContactUs />} />
         <Route path="/productoverview/:id" Component={ProductOverview}></Route>
-        <Route path='/map' element={<CustomMap adverts={[]}/>}></Route>
+        <Route path='/map' element={<CustomMap />}></Route>
       </Routes>
     </LoginContextProvider>
   );
--- client/src/components/Map/CustomMap.tsx
+++ client/src/components/Map/CustomMap.tsx
@@ -1,33 +1,56 @@
 import React, { FC } from 'react';
-import ReactMapGl, { Marker } from 'react-map-gl';
-import 'mapbox-gl/dist/mapbox-gl.css';
-import { Image } from 'react-bootstrap';
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import { Icon, LatLngExpression } from 'leaflet';
 import markerIcon from '../../assets/marker.svg';
-import { PopulatedAdvert } from '../../api/collections/advert';
+import MarkerClusterGroup from 'react-leaflet-cluster';
 
-const token =
-  'pk.eyJ1IjoibHVwczAwIiwiYSI6ImNsamxycTE5cDB4ODczZnFxd20zN3Nwa3UifQ.vfbeVhZbd__hGg3g9q-Ycg';
+export const CustomMap: FC = () => {
+  const markers = [
+    {
+      coords: [51.505, -0.09],
+      message: 'Hello World!',
+    },
+    {
+      coords: [51.51, -0.09],
+      message: 'Ciao Mondo!',
+    },
+    {
+      coords: [51.515, -0.09],
+      message: 'Halo Welt!',
+    },
+  ];
+  const customIcon = new Icon({
+    iconUrl: markerIcon,
+    iconSize: [38, 38],
+  });
 
-interface CustomMapProps {
-  adverts: PopulatedAdvert[];
-}
-
-export const CustomMap: FC<CustomMapProps> = (props) => {
   return (
-    <div style={{ width: '100vw', height: '100vh' }}>
-      <ReactMapGl
-        mapboxAccessToken={token}
-        initialViewState={{
-          longitude: 11.576124,
-          latitude: 48.137154,
-          zoom: 12,
-        }}
-        mapStyle="mapbox://styles/lups00/cljlsjqmg009601qy3s87c4g9"
-      >
-        <Marker longitude={11.576124} latitude={48.137154} anchor="bottom">
-          <Image src={markerIcon} alt="marker" width={40} height={40} />
-        </Marker>
-      </ReactMapGl>
-    </div>
+    <MapContainer
+      style={{ width: '100vw', height: '100vh' }}
+      center={[51.505, -0.09]}
+      zoom={13}
+      scrollWheelZoom={true}
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <MarkerClusterGroup chunkedLoading>
+        {markers.map((marker, index) => {
+          return (
+            <Marker
+              key={index}
+              position={marker.coords as LatLngExpression}
+              icon={customIcon}
+            >
+              <Popup>
+                <h2>{marker.message}</h2>
+              </Popup>
+            </Marker>
+          );
+        })}
+      </MarkerClusterGroup>
+    </MapContainer>
   );
 };
