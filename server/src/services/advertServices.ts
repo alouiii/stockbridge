3dad9ebe06bc25da1fb217438fef8430bcc3fa8c
Matteo Luppi <100372313+lups2000@users.noreply.github.com>
2023-07-06 13:43:35 +0200

    allows search with substrings

--- server/src/services/advertServices.ts
+++ server/src/services/advertServices.ts
@@ -97,12 +97,24 @@ export const findAllAdverts = async (
     };
   }
 
-  if (search) {
+  /*if (search) {
     queryFilter = {
       ...queryFilter,
       $text: { $search: search },
     };
+  }*/
+
+  if (search) {
+    const regex = new RegExp(search, "i"); //The "i" stands for case-insensitive matching. 
+    queryFilter = {
+      ...queryFilter,
+      $or: [
+        { description: { $regex: regex } },
+        { productname: { $regex: regex } },
+      ]
+    };
   }
+  
 
   if (radius) {
     queryFilter = {
